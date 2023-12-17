dc15fa9df7e4cbd14b1e95fde3ae9b2df0a1853d
daagii <daagii_28b@yahoo.com>
2023-12-17 15:25:37 +0800

    Shoog shiddeg bolloo

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var roundScore = 0;
 
 
 // Шооны тал 
-var dice = Math.floor(Math.random() * 6) + 1;
+
 
 // <div class="player-score" id="score-0">43</div>
 //window.document.querySelector('#score-0').textContent = dice;
@@ -22,16 +22,25 @@ var dice = Math.floor(Math.random() * 6) + 1;
 //document.querySelector('#score-1').innerHTML = '<em> Yes! </em>';
 
 //Програм эхлэхэл бэлтгэе
-document.querySelector('#score-0').textContent = 0;
-document.querySelector('#score-1').textContent = 0;
 
-document.querySelector('#current-0').textContent = 0;
-document.querySelector('#current-1').textContent = 0;
+document.getElementById("score-0").textContent = '0';
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
 
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
 
-document.querySelector(".dice").style.display = "none";
+var diceDom = document.querySelector(".dice");
 
-console.log('Шоо '+ dice);
+diceDom.style.display = "none";
+
+document.querySelector(".btn-roll").addEventListener('click', function (){
+    var diceNumber = Math.floor(Math.random() * 6) + 1;
+
+    diceDom.style.display = "block";
+    diceDom.src = 'dice-' + diceNumber + '.png';
+})
+
 
 
 
