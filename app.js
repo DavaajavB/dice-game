6fc8ce682c849134926ee031ac0c40444e3b0b23
daagii <daagii_28b@yahoo.com>
2023-12-17 16:36:41 +0800

    Тоglogchıın eeljııg solıdog bolloo

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //Тоглогчийн ээлжийг хадгалах хувьсагч
 
-var actevePlayer = 1;
+var actevePlayer = 0;
 
 //Тоглогчийн оноог хадгалах хувьсагч
 
@@ -23,7 +23,6 @@ var roundScore = 0;
 
 //Програм эхлэхэл бэлтгэе
 
-document.getElementById("score-0").textContent = '0';
 document.getElementById('score-0').textContent = '0';
 document.getElementById('score-1').textContent = '0';
 
@@ -34,12 +33,55 @@ var diceDom = document.querySelector(".dice");
 
 diceDom.style.display = "none";
 
-document.querySelector(".btn-roll").addEventListener('click', function (){
+
+//Shoog shideh event listener
+document.querySelector(".btn-roll").addEventListener("click", function () {
+
+    //1-6 дотор санассаргүй нэг тоог гаргаж авна 
     var diceNumber = Math.floor(Math.random() * 6) + 1;
 
+    //Шооны зургийг веб дээр гаргаж ирнэ
     diceDom.style.display = "block";
-    diceDom.src = 'dice-' + diceNumber + '.png';
-})
+
+    // буусан санамсаргүй тоонд харгалзах шооны зургийг веб дээр гаргаж ирнэ
+    diceDom.src = "dice-" + diceNumber + '.png';
+
+   // Буусан тоо нь 1 ээс ялгаатай бол идвхтэй тоглогчийн ээлжийн оноог нэмнэ
+    if(diceNumber !== 1) 
+    {
+        roundScore = roundScore + diceNumber;
+        document.getElementById("current-" + actevePlayer).textContent = roundScore;
+
+    }else {
+            // 1 буусан тул тоглогчийн ээлжийг солино
+            // Хэрэв идэвхитэй тоглогч нь 0 байвал 1 болго
+            // Хэрэв идэвхитэй тоглогч нь 1 байвал 0 болго
+                roundScore = 0;
+                document.getElementById("current-" + actevePlayer).textContent = 0;
+
+            // Улаан цгийг шилжүүлэх
+            document.querySelector(".player-0-panel").classList.toggle("active");
+            document.querySelector(".player-1-panel").classList.toggle("active");
+
+            // Шоог алга болгох
+            diceDom.style.display = "none";
+
+                if (actevePlayer === 0){
+                    actevePlayer = 1;
+                }else {
+                    actevePlayer = 0;
+                }
+
+
+
+
+
+
+    };
+
+
+
+});
 
 
 
